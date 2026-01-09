ad0cc16271438bea5dbd13848858e8fc33598ba0
Michelle G <mgg88@outlook.com>
2026-01-08 22:53:22 -0600

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,8 @@
 export default {
 	async fetch(request): Promise<Response> {
 		const DEFAULT_SECURITY_HEADERS = {
-			/*
-    Secure your application with Content-Security-Policy headers.
-    Enabling these headers will permit content from a trusted domain and all its subdomains.
-    @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-    "Content-Security-Policy": "default-src 'self' example.com *.example.com",
-    */
-			/*
-    You can also set Strict-Transport-Security headers.
-    These are not automatically set because your website might get added to Chrome's HSTS preload list.
-    Here's the code if you want to apply it:
-    "Strict-Transport-Security" : "max-age=63072000; includeSubDomains; preload",
-    */
-			/*
-    Permissions-Policy header provides the ability to allow or deny the use of browser features, such as opting out of FLoC - which you can use below:
-    "Permissions-Policy": "interest-cohort=()",
-    */
-			/*
-    X-XSS-Protection header prevents a page from loading if an XSS attack is detected.
-    @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
-    */
 			"X-XSS-Protection": "0",
-			/*
-    X-Frame-Options header prevents click-jacking attacks.
-    @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-    */
 			"X-Frame-Options": "DENY",
-			/*
-    X-Content-Type-Options header prevents MIME-sniffing.
-    @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-    */
 			"X-Content-Type-Options": "nosniff",
 			"Referrer-Policy": "strict-origin-when-cross-origin",
 			"Cross-Origin-Embedder-Policy": 'require-corp; report-to="default";',
@@ -81,3 +53,27 @@ export default {
 		}
 	},
 } satisfies ExportedHandler;
+
+export default {
+	async fetch(request, env, ctx) {
+		// write a key-value pair
+		await env.KV.put('KEY', 'VALUE');
+
+		// read a key-value pair
+		const value = await env.KV.get('KEY');
+
+		// list all key-value pairs
+		const allKeys = await env.KV.list();
+
+		// delete a key-value pair
+		await env.KV.delete('KEY');
+
+		// return a Workers response
+		return new Response(
+			JSON.stringify({
+				value: value,
+				allKeys: allKeys,
+			}),
+		);
+	}
+}
